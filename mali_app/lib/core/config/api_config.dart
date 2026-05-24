75ae31b09809e6b80c3b4c1fad4a4cf4d29537e1
Nlisi Malaba <nlisimalaba@gmail.com>
2026-05-24 17:44:56 +0200

    chore: update .gitignore and AndroidManifest.xml, modify API base URL
    - Updated AndroidManifest.xml to allow cleartext traffic for the application.

--- mali_app/lib/core/config/api_config.dart
+++ mali_app/lib/core/config/api_config.dart
@@ -3,6 +3,6 @@ class ApiConfig {
 
   static const String baseUrl = String.fromEnvironment(
     'MALI_API_BASE_URL',
-    defaultValue: 'http://10.0.2.2:8080',
+    defaultValue: 'http://10.0.2.2:8082',
   );
 }
